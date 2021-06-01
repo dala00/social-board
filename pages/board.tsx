15da0e5f8f9de2602baeeee5ebbeb4833f886bfc
Shoichi Namba <alphabrend@gmail.com>
2021-06-01 22:57:24 +0900

    微調整

--- pages/board.tsx
+++ pages/board.tsx
@@ -119,6 +119,8 @@ export default function Board() {
                   overId
                 )
                 setSheets(newSheets)
+              } else if (activeId === overId) {
+                setSheets(clonedSheets)
               } else {
                 const newSheets = swap(
                   sheets,
