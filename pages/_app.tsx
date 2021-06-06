afb0bfec215eae70717789750b982b5a670c8b16
Shoichi Namba <alphabrend@gmail.com>
2021-06-06 20:38:31 +0900

    認証を作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
+import { Provider } from 'next-auth/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </RecoilRoot>
+    <Provider session={pageProps.session}>
+      <RecoilRoot>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RecoilRoot>
+    </Provider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 export default function Home() {
-  return <div>index</div>
+  const [session, loading] = useSession()
+
+  return (
+    <div>
+      <div>index</div>
+      {!session && (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </div>
+  )
 }
